19d33b5aedf29106e504f5d8d566c85661b44065
priyanshu shrivastava <99priyanshu33@gmail.com>
2022-12-11 17:20:44 +0530

    added

--- basics/tut1.cpp
+++ basics/tut1.cpp
@@ -1,17 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+void swaparr(int arr[], int n){
+    for(int i =0; i<n; i=i+2){
+        if((i+1)<n){
+            swap(arr[i],arr[i+1]);
+        }
+    }
+}
+void printarr(int arr[], int n){
+    for(int i =0; i<n;++i){
+        cout<<arr[i]<<endl;
+    }
+}
 int main(){
     int arr[] = {1,2,3,4,5,6};
     int n =6;
-    for(int i =0; i<n; i=i+2){
-      
-      if((i+1)<n){
-        swap(arr[i],arr[i+1]);
-        
-      }
-
-    }
-
+   
+   swaparr(arr,n);
+   printarr(arr,n);
     return 0;
 }
